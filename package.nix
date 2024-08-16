f1e802f5b1474c924d9d7e57be973b4712545d33
Stephan <hi@polarizedions.net>
2024-08-16 20:45:18 +0200

    fix desktopitem?

--- package.nix
+++ package.nix
@@ -2,13 +2,19 @@
 
 let
   defaultVersion = import ./version.nix;
+  pname = "spplice";
+  name = pname;
+
   cfg = config.programs.spplice or { };
   version = cfg.version or defaultVersion.version;
   url = cfg.url or defaultVersion.url;
   hash = cfg.hash or defaultVersion.hash;
-in appimageTools.wrapType1 {
-  name = "spplice";
+
   src = fetchurl { inherit url hash; };
+
+  appimageContents = (appimageTools.extract { inherit pname version src; });
+in appimageTools.wrapType1 {
+  inherit pname version;
 
   nativeBuildInputs = [ copyDesktopItems ];
   desktopItems = [
@@ -21,5 +27,19 @@ in appimageTools.wrapType1 {
     })
   ];
 
+  extraInstallCommands = ''
+    # Add desktop convencience stuff
+    install -Dm444 ${appimageContents}/spplice.desktop -t $out/share/applications
+    install -Dm444 ${appimageContents}/spplice.png -t $out/share/pixmaps
+    substituteInPlace $out/share/applications/spplice.desktop \
+      --replace 'Exec=AppRun' "Exec=$out/bin/${pname}"
+  '';
+
   extraPkgs = pkgs: with pkgs; [ electron_30-bin ];
+
+  meta = with pkgs.lib; {
+    homepage = "https://p2r3.com/spplice";
+    description = "A Portal 2 mod loader";
+    platforms = platforms.linux;
+  };
 }

9ffb98e77f8e168ec91c7722d60d7547b9cf3778
Stephan <hi@polarizedions.net>
2025-04-04 21:46:48 +0200

    v0.9.2-alpha

--- version.nix
+++ version.nix
@@ -1,6 +1,6 @@
 rec {
-  version = "v0.9.1-alpha";
+  version = "v0.9.2-alpha";
   url =
     "https://github.com/p2r3/spplice-cpp/releases/download/${version}/SppliceCPP";
-  hash = "sha256-VzLbdYnwB72YAsGQSc2H0ZW1DdNqhKj/UbOAJyHv2Lg=";
+  hash = "sha256-I356fe5cLfTY0+A7CEWg5QHC+p27pCgoET4FxKNyAMo=";
 }
